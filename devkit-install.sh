b359c612a86c52c6aa81ea8d9f7bd5baa220f505
Gavin Yap <maclarensg@gmail.com>
2024-09-03 13:16:52 +0800

    save

--- devkit-install.sh
+++ devkit-install.sh
@@ -32,7 +32,7 @@ curl -o $INSTALL_DIR/Taskfile.yml.tmpl $TASKFILE_URL
 
 # Concatenate includes if exists
 if [ -f "$INSTALL_DIR/include.yaml"]; then
-  cat $INSTALL_DIR/Taskfile.yml.tmp $INSTALL_DIR/include.yml.tmpl > $INSTALL_DIR/Taskfile.yml
+  cat $INSTALL_DIR/Taskfile.yml.tmpl $INSTALL_DIR/include.yml > $INSTALL_DIR/Taskfile.yml
 else
-  cat $INSTALL_DIR/Taskfile.yml.tmp $INSTALL_DIR/Taskfile.yml 
+  cat $INSTALL_DIR/Taskfile.yml.tmpl $INSTALL_DIR/Taskfile.yml 
 fi
