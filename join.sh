2eccb3ed2392ea330e6e3c6422f778d6cd57462a
Gavin Yap <maclarensg@gmail.com>
2024-09-03 14:25:52 +0800

    save

--- join.sh
+++ join.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 INSTALL_DIR=$HOME/.local/devkit
 # Concatenate includes if exists
-if [ -f "$INSTALL_DIR/include.yml" ]; then
-  cat $INSTALL_DIR/Taskfile.yml.tmpl $INSTALL_DIR/include.yml >| $INSTALL_DIR/Taskfile.yml
+if [ -f "$INSTALL_DIR/includes.yml" ]; then
+  cat $INSTALL_DIR/Taskfile.yml.tmpl $INSTALL_DIR/includes.yml >| $INSTALL_DIR/Taskfile.yml
 else
   cat $INSTALL_DIR/Taskfile.yml.tmpl >| $INSTALL_DIR/Taskfile.yml 
 fi
